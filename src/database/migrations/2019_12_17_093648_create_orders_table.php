40419bb125b4292b1e301f2fbf0a0baf76469334
fukuba-shuma <calculus.fukuba@gmail.com>
2023-02-22 15:31:05 +0900

    fix default value

--- src/database/migrations/2019_12_17_093648_create_orders_table.php
+++ src/database/migrations/2019_12_17_093648_create_orders_table.php
@@ -39,8 +39,8 @@ class CreateOrdersTable extends Migration
             $table->unsignedInteger('delivery_method_id');
             $table->unsignedInteger('delivery_status_id');
             $table->unsignedInteger('total_price');
-            $table->tinyInteger('is_scheduled')->default(1);
-            $table->unsignedInteger('delivery_span')->default(14);
+            $table->tinyInteger('is_scheduled')->default(0);
+            $table->unsignedInteger('delivery_span')->default(14);  # day
             $table->unsignedInteger('truck_id')->nullable();
             $table->dateTime('canceled_at')->nullable();
             $table->timestamps();
