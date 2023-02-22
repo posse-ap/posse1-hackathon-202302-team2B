a7ee758b0b2b41fe8480e4133a3efca822cb8407
fukuba-shuma <calculus.fukuba@gmail.com>
2023-02-23 00:00:17 +0900

    change delivery status

--- src/resources/views/delivery/index.blade.php
+++ src/resources/views/delivery/index.blade.php
@@ -1,143 +1,121 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="delivery-index">
-        <h2>配送一覧</h2>
-        <select id="sampleSelect" onChange="goFilter();">
-            <option value="all">全て</option>
-            <option value="today_am">本日(2019/11/20) AM</option>
-            <option value="today_pm">本日(2019/11/20) PM</option>
-            <option value="tomorrow_am">明日(2019/11/20) AM</option>
-            <option value="tomorrow_pm">明日(2019/11/20) PM</option>
-        </select>
-        <ul id="sampleTable">
+<div class="delivery-index">
+    <h2>配送一覧</h2>
+    <select id="sampleSelect" onChange="goFilter();">
+        <option value="all">全て</option>
+        <option value="today_am">本日(2019/11/20) AM</option>
+        <option value="today_pm">本日(2019/11/20) PM</option>
+        <option value="tomorrow_am">明日(2019/11/20) AM</option>
+        <option value="tomorrow_pm">明日(2019/11/20) PM</option>
+    </select>
+    <ul id="sampleTable">
 
-            @foreach($orders as $order)
+        @foreach($orders as $order)
 
-                <li today_am data-bs-filter-key="20191120AM">
-                    {{ $order->getFullFormatDeliveryDateAttribute() }} 配達予定
-                    <div class="block">
-                        <div class="row position">
-                            <div class="col-5 order-head">■ 注文番号 {{ $order->id }}</div>
-                            <div class="col-7 order-head">
-                                トラック{{ $order->truck_id }}
-                                <i class="material-icons track"
-                                   data-bs-toggle="modal"
-                                   data-bs-target="#trackModal"
-                                   data-bs-whatever="track1">local_shipping</i>
-                            </div>
-                        </div>
-                        ■ 配送先住所
-                        <div class="row">
-                            <div class="col-12">〒{{ $order->delivery_address->postal_code }}</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">{{ $order->delivery_address->getFullAddressAttribute() }}</div>
-                        </div>
-                        <div class="row mb-2">
-                            <div class="col-12">{{ $order->user->company_name }}</div>
-                        </div>
-                        ■ 注文内容
-
-                        @foreach($order->order_details as $order_detail)
-
-                            <div class="row">
-                                <div class="col-5">{{ $order_detail->product->name }}</div>
-                                <div class="col-4 text-right">{{ $order_detail->quantity }}個</div>
-                                <div class="col-3 text-right">¥{{ $order_detail->product->price }}</div>
-                            </div>
-
-                        @endforeach
-
-                        <dt class="border-top pt-1">合計金額</dt>
-                        <div class="row">
-                            <div class="col-12 text-right">¥{{ $order->total_price }}</div>
-                        </div>
-
-                        <div class="text-right d-flex justify-content-between">
-                            <div>
-                            <button type="button" class="btn btn-ash"
-                                    onclick="location.href='/delivery-list/{{ $order->id }}'">
-                                詳細を見る
-                            </button>
-                                <?php
-                                $id = 6;
-                                ?>
-                                @if($id == 1)
-                                配送済
-                                @elseif($id == 2)
-                                <button type="button" class="btn btn-ash"
-                                    onclick="">
-                                    配送完了
-                                </button>
-                                </div>
-                                <p class="text-danger fs-3">
-                                    配送中
-                                </p>
-                                @elseif($id == 3)
-                                </div>
-                                <p class="text-danger fs-3">
-                                    配送前
-                                </p>
-                                @elseif($id == 4)
-                                </div>
-                                <p class="text-danger fs-3">
-                                    返品受け取り前
-                                </p>
-                                @elseif($id == 5)
-                                <button type="button" class="btn btn-ash"
-                                    onclick="">
-                                    返品完了
-                                </button>
-                                </div>
-                                <p class="text-danger fs-3">
-                                    返品受け取り中
-                                </p>
-                                @elseif($id == 6)
-                                </div>
-                                <p class="text-danger fs-3">
-                                    返品受け取り済み
-                                </p>
-                                @endif
-                        </div>
+        <li today_am data-bs-filter-key="20191120AM">
+            {{ $order->getFullFormatDeliveryDateAttribute() }} 配達予定
+            <div class="block">
+                <div class="row position">
+                    <div class="col-5 order-head">■ 注文番号 {{ $order->id }}</div>
+                    <div class="col-7 order-head">
+                        トラック{{ $order->truck_id }}
+                        <i class="material-icons track" data-bs-toggle="modal" data-bs-target="#trackModal"
+                            data-bs-whatever="track1">local_shipping</i>
                     </div>
-                </li>
-
-            @endforeach
-
-        </ul>
-
-        <!-- Modal -->
-        <div class="modal fade" id="trackModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-            <!-- //モーダルウィンドウの縦表示位置を調整・画像を大きく見せる -->
-            <div class="modal-dialog modal-lg modal-middle">
-                <div class="modal-content">
-                    <form>
-                        <div class="modal-body">
-                            <label for="trackSelect">配送トラック選択</label>
-                            <select class="form-control" id="trackSelect">
-                                <option>トラック１</option>
-                                <option>トラック２</option>
-                                <option>トラック３</option>
-                                <option>トラック４</option>
-                            </select>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-ash" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-outline-danger" data-bs-dismiss="modal" id="testttt">
-                                決定
-                            </button>
-                        </div>
-                    </form>
                 </div>
+                ■ 配送先住所
+                <div class="row">
+                    <div class="col-12">〒{{ $order->delivery_address->postal_code }}</div>
+                </div>
+                <div class="row">
+                    <div class="col-12">{{ $order->delivery_address->getFullAddressAttribute() }}</div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-12">{{ $order->user->company_name }}</div>
+                </div>
+                ■ 注文内容
+
+                @foreach($order->order_details as $order_detail)
+
+                <div class="row">
+                    <div class="col-5">{{ $order_detail->product->name }}</div>
+                    <div class="col-4 text-right">{{ $order_detail->quantity }}個</div>
+                    <div class="col-3 text-right">¥{{ $order_detail->product->price }}</div>
+                </div>
+
+                @endforeach
+
+                <dt class="border-top pt-1">合計金額</dt>
+                <div class="row">
+                    <div class="col-12 text-right">¥{{ $order->total_price }}</div>
+                </div>
+
+                <div class="text-right d-flex justify-content-between">
+                    <div class="d-flex">
+                        <button type="button" class="btn btn-ash"
+                            onclick="location.href='/delivery-list/{{ $order->id }}'">
+                            詳細を見る
+                        </button>
+                        <form action="{{route('delivery.udpate', ['order_id' => $order->id])}}" method="POST">
+                            @csrf
+                            <select name="delivery_status">
+                                <option value="1" {{ $order->delivery_status_id == 1 ? 'selected' : '' }}>準備中</option>
+                                <option value="2" {{ $order->delivery_status_id == 2 ? 'selected' : '' }}>配送中</option>
+                                <option value="3" {{ $order->delivery_status_id == 3 ? 'selected' : '' }}>配送済み</option>
+                                <option value="4" {{ $order->delivery_status_id == 4 ? 'selected' : '' }}>キャンセル中
+                                </option>
+                                <option value="5" {{ $order->delivery_status_id == 5 ? 'selected' : '' }}>キャンセル済み
+                                </option>
+                                <option value="6" {{ $order->delivery_status_id == 6 ? 'selected' : '' }}>返品申請中</option>
+                                <option value="7" {{ $order->delivery_status_id == 7 ? 'selected' : '' }}>返品配送待ち
+                                </option>
+                                <option value="8" {{ $order->delivery_status_id == 8 ? 'selected' : '' }}>返品配送中</option>
+                                <option value="9" {{ $order->delivery_status_id == 9 ? 'selected' : '' }}>返品済み</option>
+                            </select>
+                            <input type="hidden" name="order_id" value="{{ $order->id }}">
+                            <button type="submit" class="btn btn-primary">更新</button>
+                        </form>
+                    </div>
+                </div>
+        </li>
+
+        @endforeach
+
+    </ul>
+
+    <!-- Modal -->
+    <div class="modal fade" id="trackModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+        <!-- //モーダルウィンドウの縦表示位置を調整・画像を大きく見せる -->
+        <div class="modal-dialog modal-lg modal-middle">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-body">
+                        <label for="trackSelect">配送トラック選択</label>
+                        <select class="form-control" id="trackSelect">
+                            <option>トラック１</option>
+                            <option>トラック２</option>
+                            <option>トラック３</option>
+                            <option>トラック４</option>
+                        </select>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-ash" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-outline-danger" data-bs-dismiss="modal" id="testttt">
+                            決定
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push('scripts')
-    <script>
-        function goFilter() {
+<script>
+    function goFilter() {
             var wTable = document.getElementById("sampleTable");
             var wSelect = document.getElementById("sampleSelect");
             var value = wSelect.options[wSelect.selectedIndex].value;
@@ -179,5 +157,5 @@
                 $("#modalForm").modal('hide'); // モーダルを閉じる
             });
         });
-    </script>
+</script>
 @endpush
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,8 +19,11 @@ use App\Http\Controllers\DeliveryTimeController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\DriverController;
+use App\Http\Controllers\Driver\DeliveryController;
+// use App\Http\Controllers\Driver\DriverController;
 use App\Http\Controllers\ScheduledOrderController;
 use Illuminate\Support\Facades\Route;
+
 
 // 未ログイン
 Route::middleware([])->group(function () {
@@ -96,9 +99,10 @@ Route::middleware(['auth'])->group(function () {
 
     // 配送業者
     Route::middleware(['role:delivery-agent'])->group(function () {
-        Route::prefix('delivery-list')->group(function () {
-            Route::get('/', [DeliveryListController::class, 'index'])->name('delivery-list');
+        Route::controller(DeliveryController::class)->prefix('delivery-list')->group(function () {
+            Route::get('/', 'index')->name('delivery-list');
             // Route::get('/{id}', [DeliveryListDetailController::class, 'detail'])->name('delivery-list.detail');
+            Route::post('/{order_id}', 'update')->name('delivery.udpate');
         });
     });
 });
